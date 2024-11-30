0d26e87be550e5cddbfc50f68327ede4c93f2609
Vaibhav Raut <88716407+vaibhavraut07@users.noreply.github.com>
2024-12-01 02:45:37 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import Home from './components/Home'; // Import Home component
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Redirects to Home page */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
